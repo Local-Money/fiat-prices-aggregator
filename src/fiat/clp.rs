b9a89633c9e5777ec0b9d45f5701719473413878
davirds <davirds07@gmail.com>
2023-03-03 18:23:05 -0300

    add clp

--- src/fiat/clp.rs
+++ src/fiat/clp.rs
@@ -1,7 +1,7 @@
 use crate::api::buda::get_buda_price;
 use crate::api::shared::PriceValue;
 
-// retrun USDC/COP from Buda
+// retrun USDC/CLP from Buda
 pub async fn get_clp_price() -> f64 {
     let buda_price_cop = get_buda_price("USDC", "CLP").await;
     buda_price_cop.value_or_log("get_buda_price")
